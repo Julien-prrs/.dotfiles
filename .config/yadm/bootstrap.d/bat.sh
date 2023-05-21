a414984760e83f85a7b4ce258c6d72bd9a2acc44
Julien-prrs <perros.julien@outlook.fr>
2023-05-21 04:30:57 +0200

    :wrench: chore(bat): Add bootstrapping script & configuration file

---
+++ .config/yadm/bootstrap.d/bat.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+type bat &> /dev/null
+
+if [ $? -ne 0 ]; then
+   if [[ -f /etc/arch-release ]]; then
+      sudo pacman -S bat
+   elif [[ -f /etc/debian_version ]]; then
+      curl -OL https://github.com/sharkdp/bat/releases/download/v0.23.0/bat-musl_0.23.0_amd64.deb --silent
+      sudo dpkg -i bat-musl_0.23.0_amd64.deb
+      rm bat-musl_0.23.0_amd64.deb
+   fi
+fi
