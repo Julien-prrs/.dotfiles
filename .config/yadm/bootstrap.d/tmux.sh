d48615675427e40b05c5d4a9e8289f8e99d14ee8
Julien-prrs <perros.julien@outlook.fr>
2023-07-25 21:51:00 +0200

    :bug: fix (tmux): add tmux installation step

--- .config/yadm/bootstrap.d/tmux.sh
+++ .config/yadm/bootstrap.d/tmux.sh
@@ -2,6 +2,14 @@
 
 TPM_PATH="$HOME/.tmux/plugins/tpm"
 
+if [ ! -z 'tmux' ]; then
+   if [[ -f /etc/arch-release ]]; then
+      sudo pacman -Syu --noconfirm tmux
+   elif [[ -f /etc/debian_version ]]; then
+      sudo apt-get install tmux
+   fi
+fi
+
 if [ ! -d $TPM_PATH ]; then
    git clone https://github.com/tmux-plugins/tpm $TPM_PATH
 fi
