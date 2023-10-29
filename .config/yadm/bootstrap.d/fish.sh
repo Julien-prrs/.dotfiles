76193cbb1ef7a47029cfa9e8d251b2d5b620b73c
Julien-prrs <perros.julien@outlook.fr>
2023-10-29 03:09:02 +0100

    :bug: fix(fish): avoid fish related packages not to be installed

--- .config/yadm/bootstrap.d/fish.sh
+++ .config/yadm/bootstrap.d/fish.sh
@@ -5,18 +5,14 @@ OMF_INSTALL_PATH="$HOME/.local/share/omf"
 
 for package in $PACKAGES_TO_INSTALL; do
    type $package &> /dev/null
-   if [ $? -eq 0 ]; then
-      PACKAGES_TO_INSTALL=${PACKAGES_TO_INSTALL//$package/}
+   if [ $? -ne 0 ]; then
+      if [[ -f /etc/arch-release ]]; then
+         sudo pacman -Syu --noconfirm $package
+      elif [[ -f /etc/debian_version ]]; then
+         sudo apt-get install $package
+      fi
    fi
 done
-
-if [ ! -z $PACKAGES_TO_INSTALL ]; then
-   if [[ -f /etc/arch-release ]]; then
-      sudo pacman -Syu --noconfirm $PACKAGES_TO_INSTALL
-   elif [[ -f /etc/debian_version ]]; then
-      sudo apt-get install $PACKAGES_TO_INSTALL
-   fi
-fi
 
 if [ ! -d $OMF_INSTALL_PATH ]; then
    curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
