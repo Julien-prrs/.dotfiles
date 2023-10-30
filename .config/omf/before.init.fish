46618dc2d80872debe660c352270135f07a1b1f2
Julien-prrs <perros.julien@outlook.fr>
2023-10-30 03:56:48 +0100

    :technologist: :lipstick: chore(tmux): custonize tmux depending on distro
    * Use distro name as zen's sessions name
    * Use distro accent color as background for zen's sessions name

---
+++ .config/omf/before.init.fish
@@ -0,0 +1,13 @@
+if not set -q ZEN_SESSION_INITIALIZED
+   set -l GREP_MATCH (grep '^NAME=' /etc/os-release)
+   set ZEN_SESSION_NAME (string match '^NAME="(\S+).*"$' -r -g $GREP_MATCH)
+
+   switch $ZEN_SESSION_NAME
+      case Ubuntu
+         set -x ZEN_SESSION_COLOR "#ff5722"
+      case Arch
+         set -x ZEN_SESSION_COLOR "#1793d1"
+      case Manjaro
+         set -x ZEN_SESSION_COLOR "#35bfa4"
+   end
+end
