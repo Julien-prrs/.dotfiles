394157b6ddd2ca37071fe77502a7dc499cd52db1
Julien-prrs <perros.julien@outlook.fr>
2023-12-19 22:09:39 +0100

    :sparkles: feat(fish): execute `adb` from Windows when running in WSL

--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -2,5 +2,9 @@ set -U fish_greeting
 set -gx GPG_TTY (tty)
 set -U EDITOR nvim
 
+set IS_WSL grep -i Microsoft /proc/version
+
 # Aliases
 if type -q bat; alias cat="bat"; end;
+if $IS_WSL > /dev/null; alias adb="adb.exe"; end;
+
